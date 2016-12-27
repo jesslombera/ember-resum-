b43c3794a78224060ca3de368c6a439d9bd14652
Jessica Lombera <jesstlombera@gmail.com>
2016-12-27 12:53:09 -0800

    added new routes, templates and tests for home, about and index file

---
+++ tests/acceptance/resume-homepage-test.js
@@ -0,0 +1,14 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'resume-project/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | resume homepage');
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+
+
