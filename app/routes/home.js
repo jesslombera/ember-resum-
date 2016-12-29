5c6c0cc5291825f8cadab1887068df6e4a050d14
Jessica Lombera <jesstlombera@gmail.com>
2016-12-29 13:20:23 -0800

    changed background color

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
 
+	let home = [{
+		id: 'javascript',
+		title: 'Javascript',
+	}, {
+		id: 'mid1',
+		title: '-',
+	}, 
+	 {
+		id: 'html',
+		title: 'HTML',
+	},  {
+		id: 'mid2',
+		title: '-',
+	},  {
+		id: 'css',
+		title: 'CSS',
+	},  {
+		id: 'mid3',
+		title: '-',
+	},  {
+		id: 'jquery',
+		title: 'jQuery',
+	}];
+
 export default Ember.Route.extend({
+	model() {
+    	return home;
+  	}
 });
